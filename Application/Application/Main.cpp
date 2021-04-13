cda829998538c64414617fb709a469dd4e204160
Millinh Apilado <40609902+mapilado128@users.noreply.github.com>
2021-04-13 16:15:00 -0700

    Main UPDATE for putting in vector array

--- Application/Application/Main.cpp
+++ Application/Application/Main.cpp
@@ -1,69 +1,94 @@
-#include <iostream>
-#include <sstream>
 #include <string>
 #include <vector>
 #include <fstream>
-#include <cstring>
-#include <cstdio>
-//#include "cabStructx.h"
+#include <sstream>
 
-typedef struct{
-    char citySource[25]{};
-    char cityDest[25]{};
-    char cabBrand[4]{};
-    char cabType[12]{};
-    char identifier[40]{};
-    char prod_id[15]{};
-    double surgeMultiplier = 0.0;
-    int time = 0;
-    double distance = 0.0;
+using namespace std;
+
+struct Cabs{
+    string citySource;
+    string cityDest;
+    string cabBrand;
+    string cabType;
+    string identifier;
+    string prod_id;
+    double surgeMultiplier;
+    double time;
+    double distance;
     double price;
-} Cab;
-
-int j = 1;
+};
 
 int main()
 {
-    FILE *cabData;
-    cabData = fopen("F:/SP2021/CSC 340/untitled/test.csv", "r");
+    ifstream myfile;
+    //path file might need to be fixed
+    myfile.open("cab_rides.csv", ios::in);
 
-    if (cabData == NULL)
+    string line;
+
+    std::vector<Cabs> cabRides;
+
+    int lineCount = 0;
+    getline(myfile, line); // ignore header line
+
+    Cabs inv;
+
+    while (getline(myfile, line) && !line.empty())
     {
-        printf("Not successful");
-    }
+        stringstream mystream(line);
 
-    rewind(cabData);
+        string temp;
 
-    char rowLine[5000];
-    fgets(rowLine, 1024, cabData);
-    const char *split =",";
 
-    int i = 0;
-
-    for(char c = getc(cabData); c!= EOF; c=getc(cabData)){
-        if(c=='\n'){
-            j++;
+        getline(mystream, temp, ',');
+        if(temp.empty()){
+            temp = 0.000;
         }
+        else{
+            inv.distance = stod(temp);
+        }
+
+        getline(mystream, inv.cabBrand, ',');
+
+        getline(mystream, temp, ',');
+        if(temp.empty()){
+            inv.time = 0.000;
+        }
+        else {
+            inv.time = stod(temp);
+        }
+
+
+        getline(mystream, inv.cityDest, ',');
+        getline(mystream, inv.citySource, ',');
+
+        getline(mystream, temp, ',');
+        if(temp.empty()){
+            inv.price = 0.000;
+        }
+        else {
+            inv.price = stod(temp);
+        }
+
+        getline(mystream, temp, ',');
+        if(temp.empty()){
+            inv.surgeMultiplier = 0.000;
+        }
+        else {
+            inv.surgeMultiplier = stod(temp);
+        }
+
+        getline(mystream, inv.identifier, ',');
+        getline(mystream, inv.prod_id, ',');
+        getline(mystream, inv.cabType, ',');
+
+
+        if (!mystream)
+            break;
+
+        cabRides.push_back(inv);
+        lineCount++;
     }
-    Cab cabs[j+1];
-    rewind(cabData);
-
-    while(fgets(rowLine, 1024, (FILE*)cabData) != NULL){
-        cabs[i].distance = atoi(strtok(rowLine, split));
-        strcpy(cabs[i].cabBrand, strtok(NULL, split));
-        cabs[i].time = atoi(strtok(NULL, split));
-        strcpy(cabs[i].cityDest, strtok(NULL, split));
-        strcpy(cabs[i].citySource, strtok(NULL, split));
-        cabs[i].price = atoi(strtok(NULL, split));
-        cabs[i].surgeMultiplier = atoi(strtok(NULL, split));
-        strcpy(cabs[i].identifier, strtok(NULL, split));
-        strcpy(cabs[i].prod_id, strtok(NULL, split));
-        strcpy(cabs[i].cabType, strtok(NULL, split));
-        i++;
-    }
-
-    fclose(cabData);
 
 
-    return 0;
 }
