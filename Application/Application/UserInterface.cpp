f330907fee305cc6f17830981f95ec2f308d3b98
DavidYeLuo <dayeluo@galileoweb.org>
2021-04-14 13:51:34 -0700

    UserInterface is now a class
    Better suited for our needs. We need to store list in our class to display them.

--- Application/Application/UserInterface.cpp
+++ Application/Application/UserInterface.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 #include "UserInterface.h"
 
-namespace UserInterface
+
+
+
+UserInterface::UserInterface()
 {
-    std::string introOptions=
+    introOptions=
     "1 - Introduction\n"
     "2 - Learn about T test\n"
     "3 - Perform calculation on sample\n"
     "4 - About us\n"
     "q - Quit\n";
 
-    std::string sampleOptions= 
+    sampleOptions= 
     "1 - Full sample\n"
     "2 - Uber sample\n"
     "3 - Lyft sample\n"
@@ -19,121 +22,121 @@ namespace UserInterface
     "5 - Nice weather sample\n"
     "q - Quit\n";
 
-    std::string mathOptions =
+    mathOptions =
     "1 - Single sample\n"
     "2 - 2 sample T-test\n"
     "q - Quit\n";
+}
 
-    void WelcomeAndIntroduce()
+void UserInterface::WelcomeAndIntroduce()
+{
+    std::cout << "----------------    Welcome    ----------------" << std::endl;
+    std::cout << "Which data would you like to analyze?" << std::endl;
+}
+
+char UserInterface::PrintMenu(std::string options)
+{
+    char userInput;
+    
+    std::cout << "------- Menu option -------" << std::endl;
+    std::cout << options;
+
+    do
     {
-        std::cout << "----------------    Welcome    ----------------" << std::endl;
-        std::cout << "Which data would you like to analyze?" << std::endl;
+        std::cin >> userInput;
     }
+    while(!IsValidMenuOption(userInput));
 
-    char PrintMenu(std::string options)
+    return tolower(userInput);
+}
+
+bool UserInterface::IsValidMenuOption(char c)
+{
+    char input = tolower(c);
+    return input == '1' || 
+            input == '2' || 
+            input == '3' ||
+            input == '4' ||
+            input == '5' ||
+            input == 'q';
+}
+
+void UserInterface::CallMainMenuFunction(char c)
+{
+    char userInput;
+    switch(c)
     {
-        char userInput;
-        
-        std::cout << "------- Menu option -------" << std::endl;
-        std::cout << options;
-
-        do
-        {
-            std::cin >> userInput;
-        }
-        while(!IsValidMenuOption(userInput));
-
-        return tolower(userInput);
+        case '1':
+            std::cout << "FIXME: Calling Menu function 1" << std::endl;
+            break;
+        case '2':
+            std::cout << "FIXME: Calling Menu function 2" << std::endl;
+            break;
+        case '3':
+            userInput = PrintMenu(mathOptions);
+            CallMathFunction(userInput);
+            break;
+        case '4':
+            std::cout << "FIXME: Calling Menu function 4" << std::endl;
+            break;
+        case '5':
+            std::cout << "FIXME: Calling Menu function 5" << std::endl;
+            break;
+        case 'q':
+            std::cout << "Exiting the program" << std::endl;
+            break;
     }
+}
 
-    bool IsValidMenuOption(char c)
+void UserInterface::CallSampleMenuFunction(char c)
+{   
+    switch(c)
     {
-        char input = tolower(c);
-        return input == '1' || 
-                input == '2' || 
-                input == '3' ||
-                input == '4' ||
-                input == '5' ||
-                input == 'q';
+        case '1':
+            std::cout << "FIXME: Calling Menu function 1" << std::endl;
+            break;
+        case '2':
+            std::cout << "FIXME: Calling Menu function 2" << std::endl;
+            break;
+        case '3':
+            std::cout << "FIXME: Calling Menu function 3" << std::endl;
+            break;
+        case '4':
+            std::cout << "FIXME: Calling Menu function 4" << std::endl;
+            break;
+        case '5':
+            std::cout << "FIXME: Calling Menu function 5" << std::endl;
+            break;
+        case 'q':
+            std::cout << "Exiting the program" << std::endl;
+            break;
     }
+}
 
-    void CallMainMenuFunction(char c)
+void UserInterface::CallMathFunction(char c)
+{
+    char userInput;
+    switch(c)
     {
-        char userInput;
-        switch(c)
-        {
-            case '1':
-                std::cout << "FIXME: Calling Menu function 1" << std::endl;
-                break;
-            case '2':
-                std::cout << "FIXME: Calling Menu function 2" << std::endl;
-                break;
-            case '3':
-                userInput = PrintMenu(mathOptions);
-                CallMathFunction(userInput);
-                break;
-            case '4':
-                std::cout << "FIXME: Calling Menu function 4" << std::endl;
-                break;
-            case '5':
-                std::cout << "FIXME: Calling Menu function 5" << std::endl;
-                break;
-            case 'q':
-                std::cout << "Exiting the program" << std::endl;
-                break;
-        }
+        case '1':
+            userInput = PrintMenu(sampleOptions);
+            CallSampleMenuFunction(userInput);
+            break;
+        case '2':
+            userInput = PrintMenu(sampleOptions);
+            CallSampleMenuFunction(userInput);
+            break;
+        case '3':
+            std::cout << "FIXME: Calling Math function 3" << std::endl;
+            break;
+        case '4':
+            std::cout << "FIXME: Calling Math function 4" << std::endl;
+            break;
+        case '5':
+            std::cout << "FIXME: Calling Math function 5" << std::endl;
+            break;
+        case 'q':
+            std::cout << "Exiting the program" << std::endl;
+            break;
     }
-
-    void CallSampleMenuFunction(char c)
-    {   
-        switch(c)
-        {
-            case '1':
-                std::cout << "FIXME: Calling Menu function 1" << std::endl;
-                break;
-            case '2':
-                std::cout << "FIXME: Calling Menu function 2" << std::endl;
-                break;
-            case '3':
-                std::cout << "FIXME: Calling Menu function 3" << std::endl;
-                break;
-            case '4':
-                std::cout << "FIXME: Calling Menu function 4" << std::endl;
-                break;
-            case '5':
-                std::cout << "FIXME: Calling Menu function 5" << std::endl;
-                break;
-            case 'q':
-                std::cout << "Exiting the program" << std::endl;
-                break;
-        }
-    }
-
-    void CallMathFunction(char c)
-    {
-        char userInput;
-        switch(c)
-        {
-            case '1':
-                userInput = PrintMenu(sampleOptions);
-                CallSampleMenuFunction(userInput);
-                break;
-            case '2':
-                userInput = PrintMenu(sampleOptions);
-                CallSampleMenuFunction(userInput);
-                break;
-            case '3':
-                std::cout << "FIXME: Calling Math function 3" << std::endl;
-                break;
-            case '4':
-                std::cout << "FIXME: Calling Math function 4" << std::endl;
-                break;
-            case '5':
-                std::cout << "FIXME: Calling Math function 5" << std::endl;
-                break;
-            case 'q':
-                std::cout << "Exiting the program" << std::endl;
-                break;
-        }
-    }
-};
+}
--- Application/Application/UserInterface.h
+++ Application/Application/UserInterface.h
@@ -1,16 +1,31 @@
 #include <string>
+#include <vector>
+// #include "Cabs.h"
 
-namespace UserInterface
+class UserInterface
 {
-    extern std::string introOptions;
-    extern std::string sampleOptions;
-    extern std::string mathOptions;
+    public:
+    std::string introOptions;
+    std::string sampleOptions;
+    std::string mathOptions;
     void WelcomeAndIntroduce();
     char PrintMenu(std::string options);
     bool IsValidMenuOption(char c);
     void CallMainMenuFunction(char c);
     void CallSampleMenuFunction(char c);
     void CallMathFunction(char c);
+    UserInterface(); // Don't recommend using this. Should only be used for testing
+
+    // TODO: Uncomment this after cabs is implemented properly
+    // UserInterface(vector<Cabs>& fullData, vector<Cabs>& uber, vector<Cabs>& lyft, vector<Cabs>& niceWeather, vector<Cabs>& badWeather);
+
+    private:
+    // TODO:Uncomment this after Cabs is implemented properly
+    // std::vector<Cabs> fullData;
+    // std::vector<Cabs> uber;
+    // std::vector<Cabs> lyft;
+    // std::vector<Cabs> niceWeather;
+    // std::vector<Cabs> badWeather;
 };
 
 // Example1 Code:
@@ -19,9 +34,11 @@ int main()
 {
     char userOption;
 
-    UserInterface::WelcomeAndIntroduce();
+    UserInterface ui;
 
-    userOption = UserInterface::PrintMenu(UserInterface::introOptions);
-    UserInterface::CallMainMenuFunction(userOption);
+    ui.WelcomeAndIntroduce();
+
+    userOption = ui.PrintMenu(ui.introOptions);
+    ui.CallMainMenuFunction(userOption);
 }
 */
