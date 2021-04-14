e7d0308a395dffbb638af2e47c5b4f4d6ec45e41
Millinh Apilado <40609902+mapilado128@users.noreply.github.com>
2021-04-13 21:11:39 -0700

    Added getters and setters on weather

--- Application/Application/Weather.h
+++ Application/Application/Weather.h
@@ -8,6 +8,40 @@ using namespace std;
 
 class Weather{
 protected:
+public:
+    const string &getLocation() const;
+
+    void setLocation(const string &location);
+
+    double getClouds() const;
+
+    void setClouds(double clouds);
+
+    double getPressure() const;
+
+    void setPressure(double pressure);
+
+    double getWind() const;
+
+    void setWind(double wind);
+
+    double getTemperature() const;
+
+    void setTemperature(double temperature);
+
+    double getRain() const;
+
+    void setRain(double rain);
+
+    double getWtime() const;
+
+    void setWtime(double wtime);
+
+    double getHumidity() const;
+
+    void setHumidity(double humidity);
+
+protected:
     string location;
     double clouds;
     double pressure;
@@ -117,4 +151,68 @@ void Weather::sortWeather() {
         lineCount1++;
     }
 
+}
+
+const string &Weather::getLocation() const {
+    return location;
+}
+
+void Weather::setLocation(const string &location) {
+    Weather::location = location;
+}
+
+double Weather::getClouds() const {
+    return clouds;
+}
+
+void Weather::setClouds(double clouds) {
+    Weather::clouds = clouds;
+}
+
+double Weather::getPressure() const {
+    return pressure;
+}
+
+void Weather::setPressure(double pressure) {
+    Weather::pressure = pressure;
+}
+
+double Weather::getWind() const {
+    return wind;
+}
+
+void Weather::setWind(double wind) {
+    Weather::wind = wind;
+}
+
+double Weather::getTemperature() const {
+    return temperature;
+}
+
+void Weather::setTemperature(double temperature) {
+    Weather::temperature = temperature;
+}
+
+double Weather::getRain() const {
+    return rain;
+}
+
+void Weather::setRain(double rain) {
+    Weather::rain = rain;
+}
+
+double Weather::getWtime() const {
+    return wtime;
+}
+
+void Weather::setWtime(double wtime) {
+    Weather::wtime = wtime;
+}
+
+double Weather::getHumidity() const {
+    return humidity;
+}
+
+void Weather::setHumidity(double humidity) {
+    Weather::humidity = humidity;
 }
