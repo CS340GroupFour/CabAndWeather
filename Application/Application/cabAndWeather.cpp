0f85e2a504654198edf249da24c9eb72ca878547
Millinh Apilado <40609902+mapilado128@users.noreply.github.com>
2021-04-14 15:56:52 -0700

    cab And Weather source and header files

---
+++ Application/Application/cabAndWeather.cpp
@@ -0,0 +1,49 @@
+#include "cabAndWeather.h"
+
+long int cabAndWeather::getXtimes() const {
+    return xtimes;
+}
+
+void cabAndWeather::setXtimes(long int xtimes) {
+    cabAndWeather::xtimes = xtimes;
+}
+
+double cabAndWeather::getXrain() const {
+    return xrain;
+}
+
+void cabAndWeather::setXrain(double xrain) {
+    cabAndWeather::xrain = xrain;
+}
+
+double cabAndWeather::getXprice() const {
+    return xprice;
+}
+
+void cabAndWeather::setXprice(double xprice) {
+    cabAndWeather::xprice = xprice;
+}
+
+const string &cabAndWeather::getXcabBrand() const {
+    return xcabBrand;
+}
+
+void cabAndWeather::setXcabBrand(const string &xcabBrand) {
+    cabAndWeather::xcabBrand = xcabBrand;
+}
+
+const string &cabAndWeather::getXcabType() const {
+    return xcabType;
+}
+
+void cabAndWeather::setXcabType(const string &xcabType) {
+    cabAndWeather::xcabType = xcabType;
+}
+
+const string &cabAndWeather::getSourceLocation() const {
+    return sourceLocation;
+}
+
+void cabAndWeather::setSourceLocation(const string &sourceLocation) {
+    cabAndWeather::sourceLocation = sourceLocation;
+}
---
+++ Application/Application/cabAndWeather.h
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+using namespace std;
+
+class cabAndWeather {
+protected:
+    long int xtimes;
+    double xrain;
+    double xprice;
+    string xcabBrand;
+    string xcabType;
+    string sourceLocation;
+
+public:
+    long int getXtimes() const;
+    void setXtimes(long int xtimes);
+    double getXrain() const;
+    void setXrain(double xrain);
+    double getXprice() const;
+    void setXprice(double xprice);
+    const string &getXcabBrand() const;
+    void setXcabBrand(const string &xcabBrand);
+    const string &getXcabType() const;
+    void setXcabType(const string &xcabType);
+    const string &getSourceLocation() const;
+    void setSourceLocation(const string &sourceLocation);
+};
