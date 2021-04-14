2dbaf8bed4ba08c3b21280c05fb4d69bbffed132
Millinh Apilado <40609902+mapilado128@users.noreply.github.com>
2021-04-13 22:20:30 -0700

    For correction

--- Application/Application/Main.cpp
+++ Application/Application/Main.cpp
@@ -1,12 +1,28 @@
 #include "Cabs.h"
 #include "Weather.h"
+ostream & operator<<(ostream& out, const Cabs& c) {
+    out << c.getDistance() << " " << c.getCabBrand() << "" << c.getCityDest() << "" << c.getCitySource() <<
+    " " << c.getPrice() << " " << c.getSurgeMultiplier() << " " << c.getIdentifier() << " " << c.getProdId() <<
+    " " << c.getCabType() << endl;
+    //copy(c..begin(), p.phones.end(), ostream_iterator<string>(out, " "));
+    return out;
+}
+void cabIter(vector<Cabs> const &a ){
+    cout << "Uber Price: " << endl;
 
-
+    for(const auto & i : a){
+        cout << i << endl;
+    }
+}
 int main()
 {
-    Cabs c;
-    c.sortCab();
+    std::vector<Cabs> cabRides;
+    std::vector<Cabs> onlyUber;
+    std::vector<Cabs> onlyLyft;
 
-    Weather w;
-    w.sortWeather();
+    Cabs objCab;
+    objCab.sortCab(cabRides, onlyUber, onlyLyft);
+
+    cabIter(cabRides);
+
 }
